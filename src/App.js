ec8847d4755e011d903aed3a3ce3599bc08e02b8
anafmt <ana.fmtavares@gmail.com>
2023-10-24 15:22:24 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='container'>
+      <header className="App-header text-center">
       Hello
       <a href='/' className='btn btn-primary'>Again</a>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className='App-footer'>Coded by Ana FM Tavares</footer>
+    </div>
     </div>
   );
 }
