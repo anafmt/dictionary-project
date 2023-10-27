8aebeb1a693721590132f509de8d11e5a3024eaa
anafmt <ana.fmtavares@gmail.com>
2023-10-27 15:00:02 +0200

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,28 +9,36 @@ export default function Dictionary () {
 
     function search (event) {
         event.preventDefault();
-    }
-
-    function handleResponse (response) {
-        console.log(response.data.meanings[0].definition);
-        setResults(response.data);
-}
-
-    // documentation: https://www.shecodes.io/learn/apis/dictionary
+            // documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = `da34a047131b20d5faab7d8tfo459827`
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
     axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleResponse (response) {
+        setResults(response.data);
+}
 
     
     function handleKeywordChange (event) {
         setKeyword(event.target.value)
     }
 
-    return (
+    if (results) {
+        return (
         <div className="Dictionary">
         <form onSubmit={search}>
             <input type="search" autoFocus={true} onChange={handleKeywordChange} />
         </form>
         <Results results={results} />
         </div>
-)}
+)} else {
+    return (
+        <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        </form>
+        <Results results={results} />
+        </div> )
+
+}}
